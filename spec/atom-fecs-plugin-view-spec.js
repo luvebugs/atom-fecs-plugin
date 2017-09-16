6b10e2eb4a55119f9a23fb10534a3bff9aaa98e6
sun <sunxiaoxu01@baidu.com>
2017-09-16 21:21:04 +0800

    init

---
+++ spec/atom-fecs-plugin-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomFecsPluginView from '../lib/atom-fecs-plugin-view';
+
+describe('AtomFecsPluginView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
